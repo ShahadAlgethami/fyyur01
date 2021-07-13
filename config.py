dd6e40fd9474be03b5ed75b996e850229065fa11
Shahad ALgethami <algethamishahad@gmail.com>
2021-07-14 00:12:53 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,17 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Enable debug mode.
 DEBUG = True
-
+''''
 # Connect to the database
-
+CREATE DATABASE hello
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.UTF-8'
+    LC_CTYPE = 'en_US.UTF-8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+'''
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost:5432/hello'
